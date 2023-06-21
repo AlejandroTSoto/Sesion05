1569b26efc1f8a381a4558e9eb366ff0d851ab11
ALEJANDRO TOMAS <asm978@inlumine.ual.es>
2023-06-21 15:23:40 +0200

    subida ejercicio 4

--- src/ual/hmis/sesion05/ejercicio04/Ejercicio04.java
+++ src/ual/hmis/sesion05/ejercicio04/Ejercicio04.java
@@ -1,5 +1,31 @@
 	package ual.hmis.sesion05.ejercicio04;
 
 public class Ejercicio04 {
+	public static int[][] obtenerDiagonales(int[][] matriz) {
+        int n = matriz.length;
+        int[][] diagonales = new int[2][n];
+
+        for (int i = 0; i < n; i++) {
+            diagonales[0][i] = matriz[i][i]; // Diagonal principal
+            diagonales[1][i] = matriz[i][n - i - 1]; // Diagonal secundaria
+        }
+
+        return diagonales;
+    }
+
+    public static double[] sumarColumnas(double[][] matriz) {
+        int n = matriz.length;
+        double[] sumaColumnas = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            double suma = 0;
+            for (int j = 0; j < n; j++) {
+                suma += matriz[j][i];
+            }
+            sumaColumnas[i] = suma;
+        }
+
+        return sumaColumnas;
+    }
 
 }
--- test/ual/hmis/sesion05/ejercicio04/Ejercicio04Test.java
+++ test/ual/hmis/sesion05/ejercicio04/Ejercicio04Test.java
@@ -6,5 +6,24 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class Ejercicio04Test {
+	@ParameterizedTest
+    @CsvSource({
+            "1, 2, 3; 4, 5, 6; 7, 8, 9 | 1, 5, 9; 3, 5, 7",
+            "9, 8; 7, 6 | 9, 6; 8, 7"
+    })
+    void testObtenerDiagonales(int[][] matriz, int[][] expectedDiagonals) {
+        int[][] diagonals = Ejercicio04.obtenerDiagonales(matriz);
+        assertArrayEquals(expectedDiagonals, diagonals);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1.1, 2.2, 3.3; 4.4, 5.5, 6.6; 7.7, 8.8, 9.9 | 13.2, 16.5, 19.8",
+            "0.5, 1.0; 1.5, 2.0 | 2.0, 3.0"
+    })
+    void testSumarColumnas(double[][] matriz, double[] expectedSumColumns) {
+        double[] sumColumns = Ejercicio04.sumarColumnas(matriz);
+        assertArrayEquals(expectedSumColumns, sumColumns);
+    }
 
 }
